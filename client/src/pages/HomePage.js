2c5ea6d0ee13633bdabef82bc08ae0c46ffed626
Dayan <dramirez5191@gmail.com>
2019-11-21 16:07:34 -0500

    Added the Choice Slider Component into the homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import ChoiceSlider from '../components/ChoiceSlider';
+import '../styles/Slider.css';
 
 class HomePage extends React.Component {
-    render(){
-        return(
-            <h1>Main Page</h1>
-        );}
+	render() {
+		return (
+			<div>
+				<ChoiceSlider />
+			</div>
+		);
+	}
 }
-
 
 export default HomePage;
