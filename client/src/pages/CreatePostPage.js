7082dfd4d8fc9940045bc101613ea4dd86b6e1b6
Dayan <dramirez5191@gmail.com>
2019-11-23 23:40:32 -0500

    No changes since last commit (pushing to pull updates)

--- client/src/pages/CreatePostPage.js
+++ client/src/pages/CreatePostPage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import buildingBlocks from '../images/building_blocks.svg';
 
 const rightSide = {
-	marginTop: '15%'
+	marginTop: '13%',
+	paddingBottom: '88px'
 };
 const leftSide = {
 	marginTop: '10%',
-	fontSize: '52px',
+	fontSize: '48px',
 	fontFamily: 'Roboto',
 	color: 'rgba(0, 0, 0, 0.85)',
 	fontStyle: 'normal',
@@ -19,9 +20,10 @@ const paragraphStyles = {
 	fontFamily: 'Roboto',
 	fontStyle: 'normal',
 	fontWeight: 'normal',
-	fontSize: '24px',
+	fontSize: '20px',
 	lineHeight: '28px',
-	color: '#5B5B5B'
+	color: '#5B5B5B',
+	height: '56px'
 };
 class CreatePostPage extends React.Component {
 	render() {
@@ -44,6 +46,20 @@ class CreatePostPage extends React.Component {
 									src={buildingBlocks}
 								/>
 							</div>
+						</div>
+					</div>
+				</div>
+				<div className="ui form">
+					<div className="inline fields">
+						<div className="eight wide field">
+							<label>Name</label>
+							<input type="text" placeholder="First Name" />
+						</div>
+						<div className="three wide field">
+							<input type="text" placeholder="Middle Name" />
+						</div>
+						<div className="five wide field">
+							<input type="text" placeholder="Last Name" />
 						</div>
 					</div>
 				</div>
