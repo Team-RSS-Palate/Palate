a7da4e9e6b64ead222d0e4d66ebe937563db76c9
Dayan <dramirez5191@gmail.com>
2019-11-26 23:09:23 -0500

    adding validation and a file uploader for the image, work in progress

--- client/src/pages/CreatePostPage.js
+++ client/src/pages/CreatePostPage.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import '../styles/PostStyles.css';
 import CategoryList from '../components/CategoryList';
-
+import axios from 'axios';
 import buildingBlocks from '../images/building_blocks.svg';
 
 class CreatePostPage extends React.Component {
 	state = {
 		category: '',
-		categoryList: []
+		categoryList: [],
+		selectedFile: null,
+		postTitle: '',
+		postTitleError: '',
+		categoryError: ''
+	};
+	validateForm = () => {
+		let postTitleError = '';
+		let categoryError = '';
+
+		if (!this.state.postTitle) {
+			postTitleError = 'A recipe must have a name';
+		}
+		if (postTitleError) {
+			this.setState({ postTitleError });
+			return false;
+		}
+		return true;
+	};
+	onFormSubmit = (e) => {
+		e.preventDefault();
+		const isValid = this.validateForm();
+		if (isValid) {
+			console.log(this.state);
+		}
+	};
+	//update recipeName
+	onTitleChange = (e) => {
+		this.setState({ postTitle: e.target.value });
 	};
 
 	onAddItem = (e) => {
@@ -16,6 +44,14 @@ class CreatePostPage extends React.Component {
 		});
 	};
 
+	onFileSelected = (e) => {
+		this.setState({ selectedFile: e.target.files[0] });
+	};
+	// onFileUpload = (e) => {
+	// 	const fd = new FormData();
+	// 	fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
+	// 	axios.post('');
+	// };
 	render() {
 		return (
 			<div>
@@ -43,28 +79,39 @@ class CreatePostPage extends React.Component {
 				</div>
 
 				<div className="container formContainer">
-					<form className="ui big form ">
+					<form className="ui big form " onSubmit={this.onFormSubmit}>
 						<div className="fields">
 							<div className="col-sm-12 col-lg-6 field">
 								<div className="subHeading">Recipe name</div>
-								<input type="text" placeholder="Recipe Name" />
+								<input
+									value={this.state.postTitle}
+									name="recipe name"
+									onChange={this.onTitleChange}
+									type="text"
+									placeholder="Recipe Name"
+								/>
+								{this.state.postTitleError ? (
+									<div className="ui pointing red basic label">{this.state.postTitleError}</div>
+								) : null}
 							</div>
 
 							<div className="col-sm-12 col-lg-3 field">
 								<div className="subHeading">Number of servings</div>
-								<input type="text" placeholder="State" />
+								<input type="text" placeholder="Number of servings" />
 							</div>
 
 							<div className="col-sm-12 col-lg-3 field">
 								<div className="subHeading">Cook time</div>
-								<input type="text" placeholder="Zip" />
+								<input type="text" placeholder="Cook Time" />
 							</div>
 						</div>
-						<div className="fields">
-							<div className="col-sm-12 col-lg-6 field">
+
+						<div className="row">
+							<div className="col-sm-12 col-md-12 col-lg-6 field">
 								<div className="subHeading">Add tags to your recipe!</div>
 								<div className="ui action input">
 									<input
+										value={this.state.CategoryList}
 										type="text"
 										onChange={(e) => this.setState({ category: e.target.value })}
 										placeholder="Salad, Breakfast, Soup"
@@ -75,7 +122,18 @@ class CreatePostPage extends React.Component {
 								</div>
 								<CategoryList categories={this.state.categoryList} />
 							</div>
+							<div style={{ marginTop: '3%' }} className="col-sm-12 col-md-12 col-lg-6 ">
+								<div className="ui placeholder segment">
+									<div className="ui icon header">
+										<i className="file image outline icon" />
+										Recipe Image Upload
+										<input onChange={this.onFileSelected} type="file" />
+										{/* <button className="ui primary button" onClick={this.onFileUpload} /> */}
+									</div>
+								</div>
+							</div>
 						</div>
+						<button type="submit">submit</button>
 					</form>
 				</div>
 			</div>
