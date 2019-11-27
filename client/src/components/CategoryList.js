d1189cb67abfc2f5f1a927391574492bb53b4160
Dayan <dramirez5191@gmail.com>
2019-11-26 23:08:03 -0500

    minor changes

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -3,13 +3,13 @@ import React from 'react';
 const CategoryList = (props) => {
 	const categories = props.categories.map((category, index) => {
 		return (
-			<p class="ui teal label" key={index}>
+			<p className="ui label" key={index}>
 				{category}
 			</p>
 		);
 	});
 
-	return <div className="ui horizontal list ui red labels">{categories}</div>;
+	return <div className="ui horizontal list ui teal labels">{categories}</div>;
 };
 
 export default CategoryList;
