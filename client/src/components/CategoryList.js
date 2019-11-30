a5ce1a0320e4b265159c005a5bbc096a3874c37a
Dayan <dramirez5191@gmail.com>
2019-11-30 15:36:43 -0500

    added more functionality to the list, now you can add and remove

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -3,13 +3,19 @@ import React from 'react';
 const CategoryList = (props) => {
 	const categories = props.categories.map((category, index) => {
 		return (
-			<p className="ui label" key={index}>
+			<button
+				key={index}
+				className="ui labeled icon teal button"
+				type="button"
+				onClick={(e) => props.remove(category)}
+			>
+				<i className="close icon" />
 				{category}
-			</p>
+			</button>
 		);
 	});
 
-	return <div className="ui horizontal list ui teal labels">{categories}</div>;
+	return <div className="ui horizontal list ">{categories}</div>;
 };
 
 export default CategoryList;
