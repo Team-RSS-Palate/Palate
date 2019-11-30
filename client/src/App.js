34975b11a62482de1ecf59c3543c874a795d851b
Dayan <dramirez5191@gmail.com>
2019-11-30 15:36:05 -0500

    no changes, probably a space

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navagation from './components/Navagation';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  NavLink
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, NavLink } from 'react-router-dom';
 import AboutUsPage from './pages/AboutUsPage';
 import HomePage from './pages/HomePage';
 import LogInPage from './pages/LoginPage';
@@ -17,19 +11,19 @@ import Profilepage from './pages/ProfilePage';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <Navagation />
-      <Switch>
-        <Route path="/about-us" component={AboutUsPage}/>
-        <Route path="/user/signin" component={LogInPage}/>
-        <Route path="/user/signup" component={SignUpPage}/>
-        <Route path="/profile-page" component={Profilepage}/>
-        <Route path="/posts/new" component={CreatePostPage}/>
-        <Route path="/" component={HomePage}/>        
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Navagation />
+			<Switch>
+				<Route path="/about-us" component={AboutUsPage} />
+				<Route path="/user/signin" component={LogInPage} />
+				<Route path="/user/signup" component={SignUpPage} />
+				<Route path="/profile-page" component={Profilepage} />
+				<Route path="/posts/new" component={CreatePostPage} />
+				<Route path="/" component={HomePage} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
