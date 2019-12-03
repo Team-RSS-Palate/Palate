62b814466c1dcbb3549c20251c5ec1d6a1b10841
Dayan <dramirez5191@gmail.com>
2019-12-03 00:45:10 -0500

    fixed list-key issue

--- client/src/components/IngredientList.js
+++ client/src/components/IngredientList.js
@@ -3,13 +3,11 @@ import React from 'react';
 const IngredientList = (props) => {
 	const ingredients = props.ingredients.map((ingredient, index) => {
 		return (
-			<div>
+			<div key={index}>
 				<button onClick={(e) => props.remove(ingredient)}>
 					<i className="close icon" />
 				</button>
-				<p className="ui segment" key={index}>
-					{ingredient}
-				</p>
+				<p className="ui segment">{ingredient}</p>
 			</div>
 		);
 	});
