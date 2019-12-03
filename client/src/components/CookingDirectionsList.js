b55b740efce2d6363196d03c6a4e60adec730f33
Dayan <dramirez5191@gmail.com>
2019-12-03 00:44:50 -0500

    fixed list-key issue

--- client/src/components/CookingDirectionsList.js
+++ client/src/components/CookingDirectionsList.js
@@ -3,18 +3,17 @@ import React from 'react';
 const CookingDirectionsList = (props) => {
 	const steps = props.ingredients.map((step, index) => {
 		return (
-			<div>
-				<button onClick={(e) => props.remove(step)}>
-					<i className="close icon" />
-				</button>
-				<p className="ui segment" key={index}>
-					Step {index + 1} {step}
-				</p>
+			<div className="item" key={index}>
+				<i className=" close icon" onClick={(e) => props.remove(step)} />
+				<div className="content">
+					<p className="medium header">Step {index + 1}</p>
+					<p>{step}</p>
+				</div>
 			</div>
 		);
 	});
 
-	return <div>{steps}</div>;
+	return <div className="ui middle aligned divided list">{steps}</div>;
 };
 
 export default CookingDirectionsList;
