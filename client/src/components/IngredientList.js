63715b90dde383bb79fcf7c70375465033bc72d0
Dayan <dramirez5191@gmail.com>
2019-12-03 18:30:02 -0500

    added delete button

--- client/src/components/IngredientList.js
+++ client/src/components/IngredientList.js
@@ -3,16 +3,17 @@ import React from 'react';
 const IngredientList = (props) => {
 	const ingredients = props.ingredients.map((ingredient, index) => {
 		return (
-			<div key={index}>
-				<button onClick={(e) => props.remove(ingredient)}>
-					<i className="close icon" />
-				</button>
-				<p className="ui segment">{ingredient}</p>
+			<div className="item" key={index}>
+				<i className="close icon" onClick={(e) => props.remove(ingredient)} />
+				<div className="content">
+					{/* <p className="medium header">Step {index + 1}</p> */}
+					<p>{ingredient}</p>
+				</div>
 			</div>
 		);
 	});
 
-	return <div>{ingredients}</div>;
+	return <div className="ui middle aligned divided list">{ingredients}</div>;
 };
 
 export default IngredientList;
