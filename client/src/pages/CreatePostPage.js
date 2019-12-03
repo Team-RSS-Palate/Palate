926dd669b7186621bb7a50b606455665e5185cc9
Dayan <dramirez5191@gmail.com>
2019-12-03 00:46:03 -0500

    added a new function to upload image

--- client/src/pages/CreatePostPage.js
+++ client/src/pages/CreatePostPage.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import '../styles/PostStyles.css';
+import { Redirect } from 'react-router-dom';
+import { storage } from '../firebase';
+
 import CategoryList from '../components/CategoryList';
 import IngredientList from '../components/IngredientList';
 import CookingDirectionsList from '../components/CookingDirectionsList';
+// import ImageUpload from '../components/ImageUpload';
 
 import buildingBlocks from '../images/building_blocks.svg';
 import foto from '../images/undraw_searching_p5ux.png';
-import axios from 'axios';
+import '../styles/PostStyles.css';
 
 class CreatePostPage extends React.Component {
 	state = {
+		postTitle: '',
+		postTitleError: '',
+		postDescription: '',
 		category: '',
 		categoryList: [],
 		ingredient: '',
@@ -18,10 +24,41 @@ class CreatePostPage extends React.Component {
 		step: '',
 		stepsList: [],
 		ingredientsError: '',
-		selectedFile: null,
-		postTitle: '',
-		postTitleError: '',
-		categoryError: ''
+		categoryError: '',
+		success: false,
+		error: false,
+		image: '',
+		imageURL: '',
+		progress: 0
+	};
+	handleChange = (e) => {
+		const image = e.target.files[0];
+		this.setState(
+			() => ({ image }),
+			() => {
+				const { image } = this.state;
+				const uploadTask = storage.ref(`images/${image.name}`).put(image);
+				uploadTask.on(
+					'state_changed',
+					(snapshot) => {
+						// progress function ....
+						const progress = Math.round(snapshot.bytesTransferred / snapshot.totalBytes * 100);
+						this.setState({ progress });
+					},
+					(error) => {
+						// error function ....
+						console.log(error);
+					},
+					() => {
+						// complete function ....
+						storage.ref('images').child(image.name).getDownloadURL().then((url) => {
+							console.log(url);
+							this.setState({ imageURL: url });
+						});
+					}
+				);
+			}
+		);
 	};
 	validateForm = () => {
 		let postTitleError = '';
@@ -64,16 +101,14 @@ class CreatePostPage extends React.Component {
 	onTitleChange = (e) => {
 		this.setState({ postTitle: e.target.value });
 	};
+	onTitleChange = (e) => {
+		this.setState({ postDescription: e.target.value });
+	};
 	//add to the ingredients array
 	onAddIngredient = (e) => {
-		this.setState(
-			{
-				ingredientList: this.state.ingredientList.concat(this.state.ingredient)
-			},
-			() => {
-				this.setState({ ingredient: '' });
-			}
-		);
+		this.setState({ ingredientList: this.state.ingredientList.concat(this.state.ingredient) }, () => {
+			this.setState({ ingredient: '' });
+		});
 	};
 	onDeleteIngredient = (e) => {
 		const newList = this.state.ingredientList.filter((ingredient) => {
@@ -86,36 +121,23 @@ class CreatePostPage extends React.Component {
 	};
 
 	onAddStep = (e) => {
-		this.setState(
-			{
-				stepsList: this.state.stepsList.concat(this.state.step)
-			},
-			() => {
-				this.setState({ step: '' });
-			}
-		);
+		this.setState({ stepsList: this.state.stepsList.concat(this.state.step) }, () => {
+			this.setState({ step: '' });
+		});
 	};
 
 	onDeleteStep = (e) => {
 		const newList = this.state.stepsList.filter((step) => {
 			return step !== e;
 		});
-
-		this.setState({
-			stepsList: [ ...newList ]
-		});
+		this.setState({ stepsList: [ ...newList ] });
 	};
 
 	//add to the category array
 	onAddCategory = (e) => {
-		this.setState(
-			{
-				categoryList: this.state.categoryList.concat(this.state.category)
-			},
-			() => {
-				this.setState({ category: '' });
-			}
-		);
+		this.setState({ categoryList: this.state.categoryList.concat(this.state.category) }, () => {
+			this.setState({ category: '' });
+		});
 	};
 	onDeleteCategory = (e) => {
 		const newList = this.state.categoryList.filter((category) => {
@@ -127,27 +149,51 @@ class CreatePostPage extends React.Component {
 		});
 	};
 
-	//update the state to the first index in the array, which is the picture
-	onFileSelected = (e) => {
-		this.setState({ selectedFile: e.target.files[0] });
+	onSavePost = (event) => {
+		fetch('/api/posts/', {
+			//fetch data from the API || get the posts from the database
+			method: 'POST',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				title: this.state.postTitle,
+				description: this.state.description,
+				ingredients: this.state.ingredientList,
+				instructions: this.state.CookingDirectionsList,
+				image: this.state.imageURL
+			})
+		})
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				}
+				throw new Error('Content validation');
+			})
+			.then((post) => {
+				this.setState({ success: true });
+			})
+			.catch((err) => {
+				this.setState({ error: true });
+			});
 	};
-	// onFileUpload = (e) => {
-	// 	const fd = new FormData();
-	// 	fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
-	// 	axios.post('');
-	// };
 
-	show = () => {
-		console.log(this.state.ingredientList);
-	};
 	render() {
+		if (this.state.success) return <Redirect to="/" />;
+
+		let errorMessage = null;
+		if (this.state.error) {
+			errorMessage = <div className="alert alert-danger">"There was an error saving this post."</div>;
+		}
 		return (
 			<div>
+				{errorMessage}
 				<div style={{ background: '#FAFAFA' }}>
 					<div className="container">
 						<div className="row">
 							<div className="col-sm-12 col-md-12 col-lg-6">
-								<div className="header">Build your own recipe!</div>
+								<div className="heading">Build your own recipe!</div>
 								<p className="paragraphStyles">
 									Craft your own master piece and share it with the world!
 								</p>
@@ -165,7 +211,6 @@ class CreatePostPage extends React.Component {
 						</div>
 					</div>
 				</div>
-
 				<div className="container formContainer">
 					<form className="ui big form " onSubmit={this.onFormSubmit}>
 						<div className="fields">
@@ -174,7 +219,7 @@ class CreatePostPage extends React.Component {
 								<input
 									value={this.state.postTitle}
 									name="recipe name"
-									onChange={this.onTitleChange}
+									onChange={this.onTitleChange} // possibly remove this since value of it will be the title
 									type="text"
 									placeholder="Recipe Name"
 								/>
@@ -183,14 +228,15 @@ class CreatePostPage extends React.Component {
 								) : null}
 							</div>
 
-							<div className="col-sm-12 col-lg-3 field">
-								<div className="subHeading">Number of servings</div>
-								<input type="text" placeholder="Number of servings" />
-							</div>
-
-							<div className="col-sm-12 col-lg-3 field">
-								<div className="subHeading">Cook time</div>
-								<input type="text" placeholder="Cook Time" />
+							<div className="col-sm-12 col-lg-6 field">
+								<div className="subHeading">Description</div>
+								<textarea
+									rows="2"
+									name="recipe name"
+									onChange={this.onDescriptionChange} // possibly remove this since value of it will be the title
+									type="text"
+									placeholder="Describe your Recipe in a few words"
+								/>
 							</div>
 						</div>
 
@@ -241,18 +287,7 @@ class CreatePostPage extends React.Component {
 								<CookingDirectionsList remove={this.onDeleteStep} ingredients={this.state.stepsList} />
 							</div>
 						</div>
-						<div className="row">
-							<div style={{ marginTop: '3%' }} className="col-sm-12 col-md-12 col-lg-6 ">
-								<div className="ui placeholder segment">
-									<div className="ui icon header">
-										<i className="file image outline icon" />
-										Recipe Image Upload
-										<input onChange={this.onFileSelected} type="file" />
-										{/* <button className="ui primary button" onClick={this.onFileUpload} /> */}
-									</div>
-								</div>
-							</div>
-						</div>
+
 						<div className="row">
 							<div className="col-sm-12 col-md-12 col-lg-6 field">
 								<div className="subHeading">Add tags to your recipe!</div>
@@ -272,9 +307,19 @@ class CreatePostPage extends React.Component {
 								) : null}
 								<CategoryList remove={this.onDeleteCategory} categories={this.state.categoryList} />
 							</div>
+
+							<div className="col-sm-12 col-md-12 col-lg-6 ">
+								<div className="subHeading">Upload image</div>
+								<progress value={this.state.progress} max="100" />
+								<input type="file" onChange={this.handleChange} />
+								<div className="ui small rounded image">
+									<img src={this.state.imageURL} />
+								</div>
+							</div>
 						</div>
-						<button onClick={this.show} className="ui medium orange button" type="submit">
-							submit
+
+						<button className="ui medium orange button" type="submit">
+							Save Recipe
 						</button>
 					</form>
 				</div>
