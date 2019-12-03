790666a2513e8bea817629276dfd304ef0f31c61
Dayan <dramirez5191@gmail.com>
2019-12-03 00:44:24 -0500

    small changes, fixed list-key issue

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -3,15 +3,12 @@ import React from 'react';
 const CategoryList = (props) => {
 	const categories = props.categories.map((category, index) => {
 		return (
-			<button
-				key={index}
-				className="ui labeled icon teal button"
-				type="button"
-				onClick={(e) => props.remove(category)}
-			>
-				<i className="close icon" />
-				{category}
-			</button>
+			<div key={index}>
+				<button className="ui labeled icon teal button" type="button" onClick={(e) => props.remove(category)}>
+					<i className="close icon" />
+					{category}
+				</button>
+			</div>
 		);
 	});
 
