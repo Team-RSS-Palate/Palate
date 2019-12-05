cbd86ee94352f0e20a53bbf1b8cdd66fc8d9f063
Dayan <dramirez5191@gmail.com>
2019-12-04 19:25:54 -0500

    some white space was added to the about us page, and i added some pictures to the choice slider

--- client/src/components/ChoiceSlider.js
+++ client/src/components/ChoiceSlider.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import foto from '../images/undraw_searching_p5ux.png';
+import Breakfast from '../images/breakfastImage.jpg';
+import Healthy from '../images/healthyImage.jpg';
+import Burger from '../images/burgerImage.jpg';
+import Dessert from '../images/dessertImage.jpg';
+import Mexican from '../images/mexicanImage.jpg';
 
 const ChoiceSlider = () => {
 	// Create a loop that will render each slide item with its right image
@@ -7,43 +12,43 @@ const ChoiceSlider = () => {
 	return (
 		<div className="scrollingWrapper">
 			<div className="card ui raised card">
-				<div className="">
-					<img className="ui massive image" src={foto} />
+				<div className="image">
+					<img style={{ height: '240px' }} className="ui medium image" src={Breakfast} />
 				</div>
 				<div className="content">
 					<h2 className="header">Breakfast</h2>
 				</div>
 			</div>
 			<div className="card ui raised card">
-				<div className="image">
-					<img src={foto} />
+				<div className="ui medium image">
+					<img style={{ height: '240px' }} src={Healthy} />
 				</div>
 				<div className="content">
 					<h2 className="header">Healthy</h2>
 				</div>
 			</div>
 			<div className="card ui raised card">
-				<div className="image">
-					<img src={foto} />
+				<div className="ui medium image">
+					<img style={{ height: '240px' }} src={Mexican} />
 				</div>
 				<div className="content">
-					<h2 className="header">Burgers</h2>
+					<h2 className="header">Mexican</h2>
 				</div>
 			</div>
 			<div className="card ui raised card">
-				<div className="image">
-					<img src={foto} />
+				<div className="ui medium image">
+					<img style={{ height: '240px' }} src={Dessert} />
 				</div>
 				<div className="content">
 					<h2 className="header">Dessert</h2>
 				</div>
 			</div>
 			<div className="card ui raised card">
-				<div className="image">
-					<img src={foto} />
+				<div className="ui medium image">
+					<img style={{ height: '240px' }} src={Burger} />
 				</div>
 				<div className="content">
-					<h2 className="header">Dessert</h2>
+					<h2 className="header">Burger</h2>
 				</div>
 			</div>
 		</div>
--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -1,24 +1,23 @@
 import React from 'react';
 
-
 const AboutUsPage = (props) => {
-    return(
-        <div>
-            <h1>Palate</h1>
-            <div>
-                <h1>Creaters</h1>
-                <div>
-                    <h1>Tyron Samaroo</h1>
-                </div>
-                <div>
-                    <h1>Dayan Ramirez</h1>
-                </div>
-                <div>
-                    <h1>Eric Sze</h1>
-                </div>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div>
+			<h1>Palate</h1>
+			<div>
+				<h1>Creaters</h1>
+				<div>
+					<h1>Tyron Samaroo</h1>
+				</div>
+				<div>
+					<h1>Dayan Ramirez</h1>
+				</div>
+				<div>
+					<h1>Eric Sze</h1>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default AboutUsPage;
