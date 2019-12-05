d1ae12ec296c44022f586880911cc184dc9ac056
Dayan <dramirez5191@gmail.com>
2019-12-05 16:18:08 -0500

    minimal changes

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CategoryList = (props) => {
 	const categories = props.categories.map((category, index) => {
 		return (
-			<div key={index}>
+			<div className="ui horizontal list " key={index}>
 				<button className="ui labeled icon teal button" type="button" onClick={(e) => props.remove(category)}>
 					<i className="close icon" />
 					{category}
@@ -12,7 +12,7 @@ const CategoryList = (props) => {
 		);
 	});
 
-	return <div className="ui horizontal list ">{categories}</div>;
+	return <div>{categories}</div>;
 };
 
 export default CategoryList;
