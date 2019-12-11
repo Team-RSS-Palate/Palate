ca706fe12aecdd63d7ae5676b941e2f34e7167fe
Dayan <dramirez5191@gmail.com>
2019-12-10 23:27:09 -0500

    created a get route for the categories

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -10,75 +10,62 @@ const { Post } = db;
 //    POST   /posts
 //    GET    /posts/:id
 //    PUT    /posts/:id
-//    DELETE /posts/:id 
+//    DELETE /posts/:id
 
 // There are other styles for creating these route handlers, we typically
 // explore other patterns to reduce code duplication.
 // TODO: Can you spot where we have some duplication below?
 
-
-router.get('/', (req,res) => {
-  Post.findAll({})
-    .then(posts => res.json(posts));
+router.get('/', (req, res) => {
+	Post.findAll({}).then((posts) => res.json(posts));
 });
 
+router.post('/', (req, res) => {
+	let { title, description, ingredients, instructions, image, categories } = req.body;
 
-router.post("/",(req, res) => {
-    let {
-      title,
-      description,
-      ingredients,
-      instructions,
-      image,
-      categories
-    } = req.body;
-
-    Post.create({  title,
-        description,
-        ingredients,
-        instructions,
-        image,
-        categories })
-      .then(post => {
-        res.status(201).json(post);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  }
-);
-
+	Post.create({
+		title,
+		description,
+		ingredients,
+		instructions,
+		image,
+		categories
+	})
+		.then((post) => {
+			res.status(201).json(post);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
 
 router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  Post.findByPk(id)
-    .then(post => {
-      if(!post) {
-        return res.sendStatus(404);
-      }
+	const { id } = req.params;
+	Post.findByPk(id).then((post) => {
+		if (!post) {
+			return res.sendStatus(404);
+		}
 
-      res.json(post);
-    });
+		res.json(post);
+	});
 });
 
+// Query multiple posts with similar categories
+router.get('/category/:category', (req, res) => {
+	const { category } = req.params;
+	Post.findAll({ where: { categories: [ category ] } }).then((posts) => res.json(posts));
+});
 
+router.delete('/:id', (req, res) => {
+	const { id } = req.params;
+	Post.findByPk(id).then((post) => {
+		if (!post) {
+			return res.sendStatus(404);
+		}
 
-
-router.delete('/:id',
-
-  (req, res) => {
-    const { id } = req.params;
-    Post.findByPk(id)
-      .then(post => {
-        if(!post) {
-          return res.sendStatus(404)
-        }
-
-        post.destroy();
-        res.sendStatus(204);
-      });
-  }
-);
-
+		post.destroy();
+		res.sendStatus(204);
+	});
+});
 
 module.exports = router;
