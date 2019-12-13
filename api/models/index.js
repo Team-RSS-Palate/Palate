f002f965be9f54e9793a746cc354eb7f0e906d93
Tyron <tyronsamaroo@gmail.com>
2019-12-12 23:14:30 -0500

    Added option to connect to live Db

--- api/models/index.js
+++ api/models/index.js
@@ -7,11 +7,14 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const dotenv = require('dotenv');
+dotenv.config();
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  //sequelize = new Sequelize('postgres://ejxcjdbpeavhis:ce614bd843ab04df1256dbbf933ef28ede40050d92db76f2923de6006b05cd3f@ec2-50-19-114-27.compute-1.amazonaws.com:5432/d8q5v2gd1g2ip8');
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
