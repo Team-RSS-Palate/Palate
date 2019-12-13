1fee576ce99b10222957926c069da44d73e48265
Tyron <tyronsamaroo@gmail.com>
2019-12-12 23:45:18 -0500

    Trying Fix Issues

--- api/models/index.js
+++ api/models/index.js
@@ -7,8 +7,6 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-const dotenv = require('dotenv');
-dotenv.config();
 
 let sequelize;
 if (config.use_env_variable) {
